7186f0635a6584e9afabeb99b9b05d3e06636c2c
Denilson Sá Maia <denilsonsa@gmail.com>
2019-11-23 12:58:33 +0100

    Bi-directional conversion of puzzle definition and strings
    This commit was entirely written during a flight, on an Android tablet.

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,20 @@
       :colHints="sampleColHints"
       :rowHints="sampleRowHints"
     />
-    <TextareaWithLabel id="texthuman" v-model="sampleText" rows="10">
+    <TextareaWithLabel
+      id="texthuman"
+      v-model="puzzleDefinitionHumanReadable"
+      @input="inputHumanReadable"
+      rows="10"
+    >
       <template v-slot:label>Easy-to-read:</template>
     </TextareaWithLabel>
-    <TextareaWithLabel id="textsimon" v-model="sampleText" rows="10">
+    <TextareaWithLabel
+      id="textsimon"
+      v-model="puzzleDefinitionSimonTatham"
+      @input="inputSimonTatham"
+      rows="10"
+    >
       <template v-slot:label>
         <a
           href="https://www.chiark.greenend.org.uk/~sgtatham/puzzles/js/pattern.html"
@@ -24,6 +34,11 @@
     >
       <template v-slot:label>Parsed version:</template>
     </TextareaWithLabel>
+    <ul v-if="puzzleDefinitionErrors.length > 0">
+      <li v-for="(error, index) in puzzleDefinitionErrors" :key="index">
+        {{ error }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -36,6 +51,12 @@
 <script>
 import NonogramGrid from "./components/NonogramGrid.vue";
 import TextareaWithLabel from "./components/TextareaWithLabel.vue";
+import {
+  parsePuzzleText,
+  stringifyPuzzleDefinitionAsSimonTathan,
+  stringifyPuzzleDefinitionAsHumanReadable,
+  validatePuzzleDefinition,
+} from "./nonogram.js";
 
 export default {
   name: "App",
@@ -45,8 +66,13 @@ export default {
   },
   data() {
     return {
-      sampleText:
+      puzzleDefinition: null,
+      puzzleDefinitionErrors: [],
+      // '',
+      puzzleDefinitionSimonTatham:
         "15x15:1.2.6/2.3.6/4.1.5/7.4/3.4/4.2/4/4.1/6.2/5.1.2.3/3.1.5/1.5/3.5/3/5/2.1/2.2/9.1/10.1/11.1/2.1.4/1.4.1/1.2/2.1.2/2.1.2/3.1.3.1/5.3/5.4/6.5/6.5",
+      puzzleDefinitionHumanReadable: "",
+
       sampleColHints: [[0], [1, 2], [5], [4]],
       sampleRowHints: [[2], [3], [2], [1, 1, 3], [2]],
       sampleGrid: [
@@ -60,50 +86,28 @@ export default {
   },
   computed: {
     parsedTextForDebugging() {
-      return JSON.stringify(this.parsePuzzleText(this.sampleText), null, 2);
+      return JSON.stringify(this.puzzleDefinition, null, 2);
     },
   },
   methods: {
-    // TODO: Write down another (two) function(s) to generate the text from a puzzle object.
-    // TODO: Move these functions to a "lib".
-    parsePuzzleText(text) {
-      // Splitting the dimensions from the hints.
-      const [
-        ,
-        width,
-        height,
-        hintstring,
-      ] = /^\s*(\d+)\s*x\s*(\d+)\s*:?\s*([\d \t.,\r\n;\/]+)\s*$/.exec(text) || [
-        "",
-        0,
-        0,
-        "",
-      ];
-      // Spliting into each line (either row or col) of hints.
-      // Also filtering the empty lines.
-      const hintlines = hintstring
-        .split(/[\r\n;\/]+/)
-        .filter(line => line.trim());
-      // Splitting each line into each individual hint, and converting to integer.
-      const hints = hintlines.map(line =>
-        line
-          .split(/[ \t.,]+/)
-          .filter(hint => hint.trim())
-          .map(hint => parseInt(hint, 10))
+    // TODO: Replace these with vuex.
+    inputSimonTatham(value) {
+      this.puzzleDefinition = parsePuzzleText(value);
+      this.puzzleDefinitionHumanReadable = stringifyPuzzleDefinitionAsHumanReadable(
+        this.puzzleDefinition
       );
-      const colHints = hints.slice(0, width);
-      const rowHints = hints.slice(width);
-      // TODO: Detect and report errors, such as:
-      // * ERROR: (width + height) != hints.length
-      // * ERROR/WARN: sum(sum(hints) for colHints) != sum(sum(hints) for rowHints)
-      // * WARN: Dimensions outside the valid range (1..99) (99 just because of display reasons, because I don't want to display 3-digit numbers)
-      // * LATER: any(sum(hints) + hints.length - 1 > height for colHints) -> This would be easily detected when trying to solve the puzzle; so we can wait to detect/report this later.
-      return {
-        width,
-        height,
-        colHints,
-        rowHints,
-      };
+      this.puzzleDefinitionErrors = validatePuzzleDefinition(
+        this.puzzleDefinition
+      );
+    },
+    inputHumanReadable(value) {
+      this.puzzleDefinition = parsePuzzleText(value);
+      this.puzzleDefinitionSimonTatham = stringifyPuzzleDefinitionAsSimonTathan(
+        this.puzzleDefinition
+      );
+      this.puzzleDefinitionErrors = validatePuzzleDefinition(
+        this.puzzleDefinition
+      );
     },
   },
 };
---
+++ src/nonogram.js
@@ -0,0 +1,106 @@
+// These functions operate on a puzzle object.
+// The puzzle object is just a plain JavaScript object with no special prototype.
+// This was chosen because it is trivial do (de)serialize as JSON, and because
+// it just works as a reactive "data" in Vue.
+
+// Given a string, parses it into a puzzle object.
+export function parsePuzzleText(text) {
+  // Splitting the dimensions from the hints.
+  const [
+    ,
+    width,
+    height,
+    hintstring,
+  ] = /^\s*(\d+)\s*x\s*(\d+)\s*:?\s*([\d \t.,\r\n;/]+)\s*$/.exec(text) || [
+    "",
+    0,
+    0,
+    "",
+  ];
+  // Spliting into each line (either row or col) of hints.
+  // Also filtering the empty lines.
+  const hintlines = hintstring.split(/[\r\n;/]+/).filter(line => line.trim());
+  // Splitting each line into each individual hint, and converting to integer.
+  const hints = hintlines.map(line =>
+    line
+      .split(/[ \t.,]+/)
+      .filter(hint => hint.trim())
+      .map(hint => parseInt(hint, 10))
+  );
+  const colHints = hints.slice(0, width);
+  const rowHints = hints.slice(width);
+  return {
+    type: "puzzle_definition",
+    width: parseInt(width, 10),
+    height: parseInt(height, 10),
+    colHints,
+    rowHints,
+  };
+}
+
+export function stringifyPuzzleDefinitionAsSimonTathan(puzzleDefinition) {
+  const p = puzzleDefinition;
+  return (
+    `${p.width}x${p.height}:` +
+    [...p.colHints, ...p.rowHints].map(hints => hints.join(".")).join("/")
+  );
+}
+
+export function stringifyPuzzleDefinitionAsHumanReadable(puzzleDefinition) {
+  const p = puzzleDefinition;
+  return (
+    `${p.width}x${p.height}\n` +
+    [...p.colHints, ...p.rowHints].map(hints => hints.join(" ")).join("\n")
+  );
+}
+
+export function validatePuzzleDefinition(puzzleDefinition) {
+  const p = puzzleDefinition;
+  let errors = [];
+
+  // These two are essentially warnings.
+  // We limit the dimensions to 99 because this tool was designed to have at
+  // most 2 digits on each hint (for layout/display reasons). We can lift this
+  // restiction later in the future, if/when needed.
+  if (p.width > 99) {
+    errors.push(`Puzzle is too wide (${p.width} > 99).`);
+  }
+  if (p.height > 99) {
+    errors.push(`Puzzle is too tall (${p.height} > 99).`);
+  }
+
+  // These are errors:
+  if (p.width !== p.colHints.length) {
+    errors.push(
+      `Width=${p.width}, but got ${p.colHints.length} vertical hints.`
+    );
+  }
+  if (p.height !== p.rowHints.length) {
+    errors.push(
+      `Height=${p.height}, but got ${p.rowHints.length} horizontal hints.`
+    );
+  }
+
+  const sumColHints = p.colHints
+    .map(hints => hints.reduce(sumReducer))
+    .reduce(sumReducer);
+  const sumRowHints = p.rowHints
+    .map(hints => hints.reduce(sumReducer))
+    .reduce(sumReducer);
+  if (sumColHints !== sumRowHints) {
+    errors.push(
+      `The columns have a total of ${sumColHints} filled cells, while the rows have ${sumRowHints}`
+    );
+  }
+
+  return errors;
+}
+
+function sumReducer(acc, cur) {
+  return acc + cur;
+}
+
+// TODO: Write down another (two) function(s) to generate the text from a puzzle object.
+// TODO: Detect and report errors, such as:
+// * ERROR/WARN: sum(sum(hints) for colHints) != sum(sum(hints) for rowHints)
+// * LATER: any(sum(hints) + hints.length - 1 > height for colHints) -> This would be easily detected when trying to solve the puzzle; so we can wait to detect/report this later.
